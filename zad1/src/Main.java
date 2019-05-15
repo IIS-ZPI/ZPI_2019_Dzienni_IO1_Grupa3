bbd8f71ab77bc4a19c29fe3ff32a579227e1c30e
veraxys <veraxys@gmail.com>
2019-05-15 15:10:03 +0200

    zmiana id

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Operations, Team Leader: Lucat0n");
+        System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Testy, Github Id: veraxys");
     }
 }
