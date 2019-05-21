498f5b4ccac3a85f796d22218d53e966e5f26534
Lucat0n <lukasz662@gmail.com>
2019-05-21 20:58:07 +0200

    Dodany interfejs IArithmeticsDiff oraz implementacja metody Difference.

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -1,8 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main implements IArithmeticsDiff{
+    public static void main(String[] args){
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Developer, Team Leader: Lucat0n");
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Operations, Team Leader: 209450");
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Developer, GithubID: pawel10x");    
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Testy, Github Id: veraxys");
     }
+
+    @Override
+    public double Difference(double A, double B) {
+        return A-B;
+    }
+}
+
+interface IArithmeticsDiff
+{
+    double Difference(double A, double B);
 }
