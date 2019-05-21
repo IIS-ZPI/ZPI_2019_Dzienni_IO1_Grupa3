8db3c439021bc3ec29bc0d31aad455c7869a0c6b
209450 <209450@edu.p.lodz.pl>
2019-05-21 23:44:32 +0200

    Interface IArithmeticsDiv

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -4,4 +4,9 @@ public class Main {
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Developer, GithubID: pawel10x");    
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Testy, Github Id: veraxys");
     }
+
+}
+
+interface IArithmeticsDiv{
+    double Division(double A, double B);
 }
