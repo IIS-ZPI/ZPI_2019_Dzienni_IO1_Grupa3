337142f6aed3293b567e8854f445260659cd95bb
209390 <209390@edu.p.lodz.pl>
2019-05-22 14:44:03 +0200

    Komentarz 2

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -1,6 +1,9 @@
 public class Main implements IArithmeticsMult,IArithmeticsAdd,IArithmeticsDiff{
     public static void main(String[] args){
 
+        /**
+         * Druk danych teamu
+         */
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Developer, Team Leader: Lucat0n");
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Operations, Team Leader: 209450");
         System.out.println("Grupa: ZPI_2019_Dzienni_IO1_Grupa3, Rola:Developer, GithubID: pawel10x");
